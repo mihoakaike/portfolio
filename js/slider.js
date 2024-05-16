feb4768c80adb009a794afefaf7e0ae7b84884c6
mihoakaike <akaike.miho.00@gmail.com>
2024-05-17 07:53:30 +0900

    変更

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 300,//スライドのスピード。初期値は300。
+    speed: 300,//スライドのスピード。
     // slidesToShow: 2,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
