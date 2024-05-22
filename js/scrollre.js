6da72924adb5e98e143b3048013ed04dda48e5a9
mihoakaike <akaike.miho.00@gmail.com>
2024-05-23 04:05:07 +0900

    変更

--- js/scrollre.js
+++ js/scrollre.js
@@ -1,10 +1,24 @@
-$(window).on('scroll load', function(){        /* ページロード時、またはスクロールされた時*/
-  var scroll = $(this).scrollTop();            /* 現在のスクロール量を測定 */
-  var windowHeight = $(window).height();       /* ウィンドウの高さを測定 */
-  $('.fadeIn').each(function(){                /* 「fadeIn」のクラスがついているものを1つずつ確認し・・・ */
-    var cntPos = $(this).offset().top;         /* 対象の要素の上からの距離を測定 */
-    if(scroll > cntPos - windowHeight + windowHeight / 3){  /* 要素がある位置までスクロールされていたら */
-      $(this).addClass('active');              /* 「active」のクラスを付与 */
-    }
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+  var elemPos = $(this).offset().top-50; //要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll > elemPos - windowHeight + 230){
+  $(this).addClass('fadeUp');
+  // 画面内に入ったらfadeUpというクラス名を追記
+  }else{
+  $(this).removeClass('fadeUp');
+  // 画面外に出たらfadeUpというクラス名を外す
+  }
   });
-});
+  
+  
+  }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
